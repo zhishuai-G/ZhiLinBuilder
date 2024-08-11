4b7645850cd638bf889171294ab759a2b6aaa0c2
gzs <623644643@qq.com>
2024-08-11 23:00:37 +0800

    fix：实现设计态的预览按钮和顶部区域的显隐

--- src/pages/builder/designTop/index.tsx
+++ src/pages/builder/designTop/index.tsx
@@ -2,10 +2,18 @@ import { Button, message } from 'antd'
 import './style'
 import axios from 'axios'
 import { useSelector } from 'react-redux';
+import { ArrowUpOutlined } from '@ant-design/icons'
+import { useEffect, useState } from 'react'
 
 function DesignTop() {
   const comReducer = useSelector((state: any) => state.comReducer)
   const comList = JSON.parse(JSON.stringify(comReducer.comList))
+
+  const [visible, setVisible] = useState(true) // 是否显示设计区顶部
+
+  const hideDesignTop = () => {
+    setVisible(false)
+  }
 
   const savePage = async () => {
     const search = window?.location?.search || ''
@@ -18,10 +26,42 @@ function DesignTop() {
     }
   }
 
+  const saveAndPreviewPage = async () => {
+    const search = window?.location?.search || ''
+    const pageId = search.replace('?pageId=', '')
+    const res = await axios.patch(`/api/page-json/${pageId}`, { pageJson: comList }).then(res => res.data)
+    if (res?.code === 200) {
+      window?.open(`http://127.0.0.1:9090/render?pageId=${pageId}`)
+    } else {
+      message.error('页面保存失败')
+    }
+  }
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'w' || e.key === 'W') {
+        setVisible(true)
+      }
+    }
+
+    console.log(11);
+    
+    window.addEventListener('keydown', handleKeyDown)
+
+    // 移除事件监听
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+
   return (
-    <div className='designTop'>
+    <div style={visible ? {} : { display: 'none' }} className='designTop'>
       <span className='title'>ZhiLinBuilder</span>
       <Button onClick={savePage} type='primary' ghost>保存</Button>
+      <Button onClick={saveAndPreviewPage} type='primary' ghost>保存并预览</Button>
+      <div onClick={hideDesignTop} className='icon'>
+        <ArrowUpOutlined />
+      </div>
     </div>
   )
 }
